6e61e46821cf3ae1407541815adec1f00cf48c7e
tamaohome <85826245+tamaohome@users.noreply.github.com>
2026-02-15 19:30:50 +0900

    ✨ feat(models): プロパティメソッドを追加 (CSVData.header)

--- tests/models/test_csv_data.py
+++ tests/models/test_csv_data.py
@@ -1,11 +1,11 @@
 from app.models.csv_data import CSVData
 
 
-def test_CSVデータの読み込み(csv_data: CSVData) -> None:
+def test_CSVデータの読み込み(csv_data: CSVData):
     assert isinstance(csv_data, CSVData)
 
 
-def test_CSVデータの行数および列数(csv_data: CSVData) -> None:
+def test_CSVデータの行数および列数(csv_data: CSVData):
     # CSVデータの行数
     assert len(csv_data.rows) == 102
     # CSVデータの列数
@@ -14,7 +14,12 @@ def test_CSVデータの行数および列数(csv_data: CSVData) -> None:
     assert len(csv_data) == len(csv_data.rows)
 
 
-def test_CSVデータの内容(csv_data: CSVData) -> None:
+def test_CSVデータのヘッダー(csv_data: CSVData):
+    # CSVデータの列数はヘッダーの個数と同値
+    assert len(csv_data.cols) == len(csv_data.header)
+
+
+def test_CSVデータの内容(csv_data: CSVData):
     # 自身のインデックス参照は rows のインデックス参照を返す
     for i in range(len(csv_data)):
         assert csv_data[i] is csv_data.rows[i]
@@ -25,16 +30,16 @@ def test_CSVデータの内容(csv_data: CSVData) -> None:
     assert csv_data.rows[42] == ["#3レベル名", "材質", "形状", "寸法", "合計", "主桁", "横桁", "横構"]
 
 
-def test_CSVデータをテーブル毎に取得(csv_tables: list[CSVData]) -> None:
+def test_CSVデータをテーブル毎に取得(csv_tables: list[CSVData]):
     # テーブルの総数
     assert len(csv_tables) == 8
 
 
-def test_CSVデータの行列取得(csv_tables: list[CSVData]) -> None:
+def test_CSVデータの行列取得(csv_tables: list[CSVData]):
     assert csv_tables[2][0] == ["#3レベル名", "材質", "形状", "寸法", "合計", "主桁", "横桁", "横構"]
     assert csv_tables[-1].cols[1] == ["材質", "SS400", "加工鋼重中計", "SS400", "購入部品中計", "合計"]
 
 
-def test_CSVテーブルリストのレベル名(csv_tables: list[CSVData]) -> None:
+def test_CSVテーブルリストのレベル名(csv_tables: list[CSVData]):
     level_names = [table.rows[0][0][:2] for table in csv_tables]
     assert level_names == ["#1", "#2", "#3", "#3", "#4", "#4", "#4", "#4"]
